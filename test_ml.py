33cce54b1dcdddaa0af413b820e925212d2ee35a
aecke28-cmd <aecke28@wgu.edu>
2026-07-13 20:31:54 -0400

    added unit tests

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,44 @@
 import pytest
-# TODO: add necessary import
+from unittest.mock import patch
+from unittest.mock import MagicMock
+import pandas as pd
 
+# TODO: add necessary import
+@patch('train_model.data')
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_nan(data):
     """
-    # add description for the first test
+    # checks that null values do not exceed 30% of the dataset
     """
     # Your code here
+    assert data.isna().mean() * (100).__lt__(30)
     pass
 
 
+@patch('train_model.p')
+@patch('train_model.r')
+@patch('train_model.fb')
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_metrics(p, r, fb):
     """
-    # add description for the second test
+    # ascertains that the precision, recall and f1 score are all between 0 and 1
     """
     # Your code here
+    min = 0
+    max = 1
+    assert min.__lt__(p) and p.__lt__(max)
+    assert min.__lt__(r) and r.__lt__(max)
+    assert min.__lt__(fb) and fb.__lt__(max)
     pass
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_age():
     """
-    # add description for the third test
+    # makes sure that the age value in the dataset is a possible value
     """
     # Your code here
+    data = pd.read_csv('data/census.csv')
+    assert data['age'].min() >= 0
+    assert data['age'].max() <= 115
     pass
